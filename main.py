bcfa6b59dab1cdf61957d7f4c1f9deb78387c941
esaiesec <esuarez@aiesec.org.mx>
2017-04-07 09:35:05 -0500

    config and git ignore added

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def main ():
 
 	print ESPO(config.CRM_USER,config.CRM_PASSWD,test = False).token
 	#tokeng =  GIS()
-	#print tokeng.generate_token('enrique.suarez@aiesec.net','si no leo me aburro')
-	#print tokeng.generate_op_token('enrique.suarez@aiesec.net','si no leo me aburro')
+	#print tokeng.generate_token('enrique.suarez@aiesec.net','')
+	#print tokeng.generate_op_token('enrique.suarez@aiesec.net','')
 	
 if __name__ == '__main__':
 	main()
